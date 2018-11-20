6e6640d4cc3eee367d1b78ff2ad5fd8a709c8df7
Yu Zhao <yu.zhao@joudou.com>
2018-11-20 18:48:25 +0800

    initial commit

---
+++ chap2.rkt
@@ -0,0 +1,35 @@
+
+
+(sqrt 2)
+
+(- #i1.0 #i0.9)
+
+(- 1.0 0.9)
+
+
+(define (sqr x)
+  (* x x))
+
+(sqr 2)
+
+(define (area-of-disk r)
+  (* 3.14 (sqr r)))
+
+(define (area-of-ring outer inner)
+  (- (area-of-disk outer)
+     (area-of-disk inner)))
+
+
+  (+ 2 2)
+    (* 3 3)
+    (- 4 2)
+(/ 6 2)
+
+(symbol= 'red 'red)
+
+(number? 10)
+
+(reply 'HowAreYou?)
+
+(make-posn 3 4)
+
